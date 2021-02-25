3a51e5b753b3bfea2c771f6561965f100c70c734
Oscar Shoudt <shoudto@gmail.com>
2021-02-25 10:03:38 -0500

    first commit

---
+++ src/data/projects.js
@@ -0,0 +1,30 @@
+import project1 from '../assets/project1.png';
+import project2 from '../assets/project2.png';
+import project3 from '../assets/project3.png';
+
+// Screen Case Syntax
+const PROJECTS = [
+    {
+        id: 1,
+        title: 'Travel-Project',
+        description: 'A Front End experience that I built, involving HTML and CSS!',
+        link: 'https://github.com/shoudto/Travel-Project',
+        image: project1
+    },
+    {
+        id: 2,
+        title: 'Beer-Log',
+        description: 'A Beer log application with access to your personal beer log!',
+        link: 'https://github.com/shoudto/Beer-Log',
+        image: project2
+    },
+    {
+        id: 3,
+        title: 'Fashion-Project',
+        description: 'Created a Front-End Design using Javascript, HTML, and CSS.',
+        link: 'https://github.com/shoudto/Fashion-Project',
+        image: project3
+    }
+];
+
+export default PROJECTS;
---
+++ src/data/socialProfiles.js
@@ -0,0 +1,24 @@
+import emailIcon from '../assets/email_icon.png';
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+
+// Global values
+const SOCIAL_PROFILES = [
+    {
+        id: 1,
+        link: 'mailto:shoudto@gmail.com',
+        image: emailIcon
+    },
+    {
+        id: 2, 
+        link: 'https://github.com/shoudto',
+        image: githubIcon
+    },
+    {
+        id: 3,
+        link: 'https://www.linkedin.com/in/shoudt/',
+        image: linkedinIcon
+    }
+];
+
+export default SOCIAL_PROFILES;
