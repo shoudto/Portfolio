acf81ffde8f96867d7da5f2b7772f09a2f6684b2
Oscar Shoudt <shoudto@gmail.com>
2021-02-25 12:49:11 -0500

    Adding more projects and styling

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,9 @@
 import project1 from '../assets/project1.png';
 import project2 from '../assets/project2.png';
 import project3 from '../assets/project3.png';
+import project4 from '../assets/project4.png';
+import project5 from '../assets/project5.png';
+import project6 from '../assets/project6.png';
 
 // Screen Case Syntax
 const PROJECTS = [
@@ -24,6 +27,27 @@ const PROJECTS = [
         description: 'Created a Front-End Design using Javascript, HTML, and CSS.',
         link: 'https://github.com/shoudto/Fashion-Project',
         image: project3
+    },
+    {
+        id: 4, 
+        title: 'BeatBox-App',
+        description: 'Your able to mix sound and create original mix!',
+        link: 'https://github.com/shoudto/BeatBox-App',
+        image: project4
+    },
+    {
+        id: 5, 
+        title: 'Photon-App',
+        description: 'Photo Website using Pexels api server. Using Javascript, HTML and CSS.',
+        link: 'https://github.com/shoudto/Photon-App',
+        image: project5
+    },
+    {
+        id: 6, 
+        title: 'Color-Project-App',
+        description: 'Generate random colors or create a custom color.',
+        link: 'https://github.com/shoudto/Color-Project-App',
+        image: project6
     }
 ];
 
